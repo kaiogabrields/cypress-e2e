2b4ed99ec30f2fa0a7d0d9452d7199bdd37fd808
Kaio Gabriel <kaiogabrields@hotmail.com>
2023-09-30 14:57:01 -0300

    adicionando fluxo crud em commands

--- cypress_e2e/cypress/e2e/crud.cy.js
+++ cypress_e2e/cypress/e2e/crud.cy.js
@@ -1,47 +1,21 @@
 import { faker } from '@faker-js/faker/locale/en'
 describe('CRUD', () => {
     it('CRUDs a note', () => {
-//create
         const noteDescription = faker.lorem.words(4)
 
         cy.intercept('GET', '**/notes').as('getNotes')
-        cy.intercept('GET', '**/notes/**').as('getNote')
         cy.sessionLogin()
-
-        cy.visit('/notes/new')
-        cy.get('#content').type(noteDescription)
-        cy.contains('button', 'Create').click()
-
+    
+        cy.createNote(noteDescription)
         cy.wait('@getNotes')
-        cy.contains('.list-group-item', noteDescription)
-            .should('be.visible')
-            .click()
-        cy.wait('@getNote')
-//update
+    
         const updatedNoteDescription = faker.lorem.words(4)
-
-        cy.get('#content')
-            .as('contentField')
-            .clear()
-        cy.get('@contentField')
-            .type(updatedNoteDescription)
-        cy.contains('button', 'Save').click()
+        const attachFile = true
+    
+        cy.editNote(noteDescription, updatedNoteDescription, attachFile)
         cy.wait('@getNotes')
-        cy.contains('.list-group-item', noteDescription).should('not.exist')
-//delete
-        cy.contains('.list-group-item', updatedNoteDescription)
-            .should('be.visible')
-            .click()
-        cy.wait('@getNote')
-        cy.contains('button', 'Delete').click()
-        cy.wait('@getNotes')
-
-        cy.get('.list-group-item')
-            .its('length')
-            .should('be.at.least', 1)
-        cy.contains('.list-group-item', updatedNoteDescription)
-            .should('not.exist')
-    })
-
-
+    
+        cy.deleteNote(updatedNoteDescription)
+        cy.wait('@getNotes')    
+      })
 })
